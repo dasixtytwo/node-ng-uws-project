4b6bfac72c09d5e4f47d9af0668997e4b77492bb
dasixtytwo <davide.a@davideagosti.com>
2020-12-28 17:15:50 +0000

    Update api services

--- server.js
+++ server.js
@@ -40,7 +40,7 @@ db.mongoose
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, "client/build"))); // Provide static directory for frontend, like CSS, JS
 // simple route
-app.get("/*", (req, res) => {
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
